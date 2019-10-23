357181c12a02943c664da3197bc582b70e88438b
Schalldampfer <ahtosi.game@gmail.com>
2019-10-23 22:44:19 +0900

    revert install pack

--- Server_Install_Pack/@epochhive/epochconfig.hpp
+++ Server_Install_Pack/@epochhive/epochconfig.hpp
@@ -1,39 +1,39 @@
-serverRestart         = "true"; 		// true sends #shutdown command to server after the time specified in ForceRestartTime
+serverRestart         = "false"; 		// true sends #shutdown command to server after the time specified in ForceRestartTime
 forceRestartTime      = 14400; 			// 4 hour restarts
 
 // Time based
 	StaticDateTime[] 	= {}; 			// {0,0,0,8,0} would forces the server to start at 8am each time it is started while allowing the year, month and day to stay real time. Any values left at 0 will result in no change.
-	timeDifference 		= -6; 			// Server uses real time this will allow you to offset just the hour.
-	timeMultiplier 		= 6; 			// Sets a time multiplier for in-game time. The command range is now capped at 0.1 - 120 to avoid performance problems.
-	lootMultiplier 		= 0.8; 			// 1 = max loot bias. This controls how much loot can payout per Epoch loot container.
+	timeDifference 		= 0; 			// Server uses real time this will allow you to offset just the hour.
+	timeMultiplier 		= 4; 			// Sets a time multiplier for in-game time. The command range is now capped at 0.1 - 120 to avoid performance problems.
+	lootMultiplier 		= 0.5; 			// 1 = max loot bias. This controls how much loot can payout per Epoch loot container.
 
 // Events
 	WeatherChances[] = {
-		0.4,							// Sun
-		0.4,							// Mid
+		0.5,							// Sun
+		0.3,							// Mid
 		0.1,							// Rain
 		0.1								// Rain+
 	};
 	events[] = {
-		//{
-		//	3600, // SECOND <scalar>,
-		//	"CarnivalSpawner", // EVENT <string>
-		//	0, //INIT <scalar> 1 = run script at startup or 0 normal delay
-		//	2, //PREPOSTFIX <scalar> 1 = (epoch_settings pbo) 2 = (epoch_events pbo) 0 = use full file path
-		//	-1, //RUNNUMTIMES <scalar> -1 infinite
-		//	{}, //execVM payload <array>
-		//	{"VR"} //disallowed worlds <array>
-		//}, 
-		{ 1800, "PaydayEvent", 0, 2},
-		{ 1200, "MessageServer", 0, 2},
-		//{ 2700, "AirDrop", 0 , 2, -1, {} ,{"VR"}},
-		//{ 2400, "EarthQuake", 0 , 2, -1, {} ,{"VR"}},
-		//{ 2700, "Satellite", 0 , 2, -1, {} ,{"VR"}},
+		{
+			3600, // SECOND <scalar>,
+			"CarnivalSpawner", // EVENT <string>
+			0, //INIT <scalar> 1 = run script at startup or 0 normal delay
+			2, //PREPOSTFIX <scalar> 1 = (epoch_settings pbo) 2 = (epoch_events pbo) 0 = use full file path
+			-1, //RUNNUMTIMES <scalar> -1 infinite
+			{}, //execVM payload <array>
+			{"VR"} //disallowed worlds <array>
+		}, 
+		// { 1800, "PaydayEvent", 0, 2},
+		// { 1200, "MessageServer", 0, 2},
+		{ 2700, "AirDrop", 0 , 2, -1, {} ,{"VR"}},
+		{ 2400, "EarthQuake", 0 , 2, -1, {} ,{"VR"}},
+		{ 2700, "Satellite", 0 , 2, -1, {} ,{"VR"}},
 		{ 900, "ChangeWeather", 1 , 2, -1, {} ,{"VR"}},
-		//{ 1200, "ContainerSpawner", 0 , 2, -1, {} ,{"VR"}},
+		{ 1200, "ContainerSpawner", 0 , 2, -1, {} ,{"VR"}},
 		{ 1440, "GardenManager", 0 , 2, -1, {} ,{"VR"}},
 		{ 1440, "SolarChargerManager", 0 , 2, -1, {} ,{"VR"}},
-		// { 900, "MoneyDrop", 0 , 2 , -1, {} ,{"VR"}},
+		{ 900, "MoneyDrop", 0 , 2 , -1, {} ,{"VR"}},
 		{ 300, "PlantSpawner", 0 , 2 , -1, {} ,{"VR"}} //No comma on last Entry
 	};
 
@@ -50,13 +50,13 @@ forceRestartTime      = 14400; 			// 4 hour restarts
 	defaultUniformMale =		"U_Test1_uniform";
 	defaultVestMale =			"V_41_EPOCH";
 	defaultGoggles =			"";
-	defaultHeadgear =			"H_Bandanna_surfer_grn";
+	defaultHeadgear =			"";
 	defaultBackpack =			"";
-	defaultprimaryWeapon[] =	{"Rollins_F","","","optic_Aco",{"5Rnd_rollins_mag",5},{},""};		// {"arifle_MX_pointer_F","","acc_pointer_IR","",{"30Rnd_65x39_caseless_mag",29},{},""};
+	defaultprimaryWeapon[] =	{};		// {"arifle_MX_pointer_F","","acc_pointer_IR","",{"30Rnd_65x39_caseless_mag",29},{},""};
 	defaultsecondaryWeapon[] = 	{};		// {"launch_NLAW_F","","","",{"NLAW_F",1},{},""};
 	defaulthandgunWeapon[] =	{};		// {"hgun_P07_F","","","",{"16Rnd_9x21_Mag",16},{},""};
-	defaultuniformItems[] =		{{"5Rnd_rollins_mag",6,5},{"Chemlight_blue",1,1}};		// {{"FAK",1},{"30Rnd_65x39_caseless_mag",2,30},{"Chemlight_green",1,1}};
-	defaultvestItems[] =		{{"ItemBottlePlastic_Clean",1},{"sweetcorn_epoch",2},{"FAK",2}};		// {{"30Rnd_65x39_caseless_mag",3,30},{"16Rnd_9x21_Mag",2,16},{"SmokeShell",1,1},{"SmokeShellGreen",1,1},{"SmokeShellBlue",1,1},{"SmokeShellOrange",1,1},{"Chemlight_green",1,1}};
+	defaultuniformItems[] =		{};		// {{"FAK",1},{"30Rnd_65x39_caseless_mag",2,30},{"Chemlight_green",1,1}};
+	defaultvestItems[] =		{};		// {{"30Rnd_65x39_caseless_mag",3,30},{"16Rnd_9x21_Mag",2,16},{"SmokeShell",1,1},{"SmokeShellGreen",1,1},{"SmokeShellBlue",1,1},{"SmokeShellOrange",1,1},{"Chemlight_green",1,1}};
 	defaultbackpackItems[] =	{};		// {{"Medikit",1},{"FAK",10},{{"hgun_P07_F","","","",{"16Rnd_9x21_Mag",16},{},""},1}};
 	defaultassignedItems[] =	{};		// {"Rangefinder","","","",{},{},""}
 	defaultlinkedItems[] =		{
@@ -75,15 +75,15 @@ forceRestartTime      = 14400; 			// 4 hour restarts
 	disableFuelNearPlots = "false";		// Removes auto refuel in PlotPole-Ranges at server startup.
 	VehLockMessages = "true";			// Give players a hint, that the Vehicle is locked / unlocked
 	removevehweapons[] = {				// remove these Weapons from spawned Vehicles
-		//"missiles_DAR","gatling_30mm","gatling_20mm","missiles_DAGR","cannon_30mm_Plane_CAS_02_F","Missile_AA_03_Plane_CAS_02_F","Missile_AGM_01_Plane_CAS_02_F","Rocket_03_HE_Plane_CAS_02_F",
-		//"Rocket_03_AP_Plane_CAS_02_F","Bomb_03_Plane_CAS_02_F","Bomb_04_Plane_CAS_01_F","Rocket_04_AP_Plane_CAS_01_F","Rocket_04_HE_Plane_CAS_01_F","Missile_AGM_02_Plane_CAS_01_F","Missile_AA_04_Plane_CAS_01_F",
-		//"Gatling_30mm_Plane_CAS_01_F","GBU12BombLauncher_Plane_Fighter_03_F","missiles_ASRAAM","missiles_SCALPEL","Twin_Cannon_20mm","missiles_Zephyr","rockets_skyfire","gatling_30mm_vtol_02"
+		"missiles_DAR","gatling_30mm","gatling_20mm","missiles_DAGR","cannon_30mm_Plane_CAS_02_F","Missile_AA_03_Plane_CAS_02_F","Missile_AGM_01_Plane_CAS_02_F","Rocket_03_HE_Plane_CAS_02_F",
+		"Rocket_03_AP_Plane_CAS_02_F","Bomb_03_Plane_CAS_02_F","Bomb_04_Plane_CAS_01_F","Rocket_04_AP_Plane_CAS_01_F","Rocket_04_HE_Plane_CAS_01_F","Missile_AGM_02_Plane_CAS_01_F","Missile_AA_04_Plane_CAS_01_F",
+		"Gatling_30mm_Plane_CAS_01_F","GBU12BombLauncher_Plane_Fighter_03_F","missiles_ASRAAM","missiles_SCALPEL","Twin_Cannon_20mm","missiles_Zephyr","rockets_skyfire","gatling_30mm_vtol_02"
 	};
 	removevehmagazinesturret[] = {		// Remove these Magazines from the given Turret from spawned Vehicles
-		//{"24Rnd_missiles",{-1}},
-		//{"200Rnd_40mm_G_belt",{0}}
+		{"24Rnd_missiles",{-1}},
+		{"200Rnd_40mm_G_belt",{0}}
 	};
-	disableVehicleTIE = "false";
+	disableVehicleTIE = "true";
 
 // BaseBuilding
 	StorageSlotsLimit 	= 1500; 		// Max storage slots allowed. Warning! Higher the number lower performance.
@@ -148,10 +148,10 @@ forceRestartTime      = 14400; 			// 4 hour restarts
 		{"ItemSodaBurst","meatballs_epoch","MortarBucket","CinderBlocks","VehicleRepair","CircuitParts","ItemCorrugated","PartPlankPack","ItemRock","ItemRope","ItemStick"},
 		{5,5,5,5,5,5,5,5,5,5,5}
 	};
-	NPCSlotsLimit = 5; 				// Max number of traders static or dynamic. Warning! Higher the number lower performance.
+	NPCSlotsLimit = 30; 				// Max number of traders static or dynamic. Warning! Higher the number lower performance.
 	forceStaticTraders = "true"; 		// disables traders moving from work to home
-	TraderGodMode = "true";			// If true, Trader can not be killed by Players
-	storedVehicleLimit = 12;			// Vehicles more than x stored in ALL Traders will automatically be deleted on Restart. 
+	TraderGodMode = "false";			// If true, Trader can not be killed by Players
+	storedVehicleLimit = 15;			// Vehicles more than x stored in ALL Traders will automatically be deleted on Restart. 
 	StaticTraderItemPurge[] = {100,15};	// {ItemCount,ReducePercent} - If a static trader have more than x different items, on restart the items will be reduced by y percent
 	DynamicTraderRespawnCount = 100;	// If a dynamic Trader have more than x different Items, he will respawn on another Spot (with start Items)
 	TraderItemCountPerItem[] = {50,5};	// If the Trader has more than x pieces of an Item, it will be reduced to y pieces (on Restart)
@@ -169,8 +169,8 @@ forceRestartTime      = 14400; 			// 4 hour restarts
 	TraderDeco = "true";				// If true, Traders get around Halloween / Christmas some seasonal decoration
 
 // Spawntables
-	forcedVehicleSpawnTable = "allowedVehiclesList"; 		// leave blank for default. Options: "allowedVehiclesList","allowedVehiclesList_CUP","allowedVehiclesList_MAD","allowedVehiclesList_MADCUP"
-	forcedLootSpawnTable = "CfgLootTable"; 			// leave blank for default. Options: "CfgLootTable","CfgLootTable_CUP","CfgLootTable_MAD","CfgLootTable_MADCUP"
+	forcedVehicleSpawnTable = ""; 		// leave blank for default. Options: "allowedVehiclesList","allowedVehiclesList_CUP","allowedVehiclesList_MAD","allowedVehiclesList_MADCUP"
+	forcedLootSpawnTable = ""; 			// leave blank for default. Options: "CfgLootTable","CfgLootTable_CUP","CfgLootTable_MAD","CfgLootTable_MADCUP"
 
 // Markers
 	showEarthQuakes = "true"; 			// show mineral viens caused by earthquakes
@@ -182,7 +182,7 @@ forceRestartTime      = 14400; 			// 4 hour restarts
 	DEBUG_VEH = "false"; 				// DEBUG ONLY used to debug spawing of vehicles
 
 // Hive Related
-	vehicleLockTime = "3600"; 			// Controls how many seconds it takes to allow another person/group to unlock vehicle.
+	vehicleLockTime = "1800"; 			// Controls how many seconds it takes to allow another person/group to unlock vehicle.
 	expiresBuilding = "604800";  		// expiration date in seconds for buildings
 	expiresPlayer = "2592000";  		// expiration date in seconds for players
 	expiresBank = "7776000";  			// expiration date in seconds for players bank
