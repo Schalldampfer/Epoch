19f619f4253fe08565c8643e68dd40329b3b9cbe
Schalldampfer <ahtosi.game@gmail.com>
2019-11-14 20:00:39 +0900

    Update CfgRemoteExec.hpp
    Advanced Towing

--- Sources/epoch_config/Configs/CfgRemoteExec.hpp
+++ Sources/epoch_config/Configs/CfgRemoteExec.hpp
@@ -391,6 +391,15 @@ class CfgRemoteExec
 			allowedTargets = 2;
 			jip = 0;
 		};
+		class SA_Simulate_Towing { allowedTargets=0; };
+		class SA_Attach_Tow_Ropes { allowedTargets=0; };
+		class SA_Take_Tow_Ropes { allowedTargets=0; };
+		class SA_Put_Away_Tow_Ropes { allowedTargets=0; };
+		class SA_Pickup_Tow_Ropes { allowedTargets=0; };
+		class SA_Drop_Tow_Ropes { allowedTargets=0; };
+		class SA_Set_Owner { allowedTargets=2; };
+		class SA_Hint { allowedTargets=1; };
+		class SA_Hide_Object_Global { allowedTargets=2; };
     };
     class Commands
     {
